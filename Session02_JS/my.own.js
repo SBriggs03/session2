2475ac1e6671e5b25fd5d8cea5147bb9478bea02
SBriggs03 <55232694+SBriggs03@users.noreply.github.com>
2019-09-19 11:42:05 +0100

    session_02

---
+++ Session02_JS/my.own.js
@@ -0,0 +1,53 @@
+function football()
+{
+    World Cup = "This is where different countries compete for a trophy"
+    Premier League = "this is where English teams compete for a trophy"
+    Europa League = "This is where teams in Europe compete for a trophy"
+    France Ligue 1 = "This is the league where French clubs ompete for a trophy"
+    Scottish Premiership = "This is where teams in te south of England compete for a trophy"
+    La Liga = "This is where teams in Spain compete for a trophy"
+    Serie A = "This is where teams in Italy compete for a trophy"
+    Bundesliga = "This is where teams in Germany compete for a trophy"
+
+    operator = prompt ("please enter one of the following: World Cup, Premier League or Europa League")
+
+    if (operator -- "World Cup") {
+       op1 - "You have selected World Cup, "
+       document.getElementById(football) .innerHTML - op1 + World Cup
+    }
+
+else {
+if (operator == "Premier League") (
+   op2 = "You have selected Premier League, "
+   document.getElementById(football).innerHTML = op2 + Premier League
+
+   else {
+    if ) (operator -- "Europa League") (
+          op3 = "You have selected Europa League, "
+          document.getElementById("football") .innerHTML - op3 + Europa League
+
+  else {
+    if ) (operator -- "France Ligue 1") (
+         op4 = "You have selected France League 1, "
+        document.getElementById("football") .innerHTML - op4 + France Ligue 1
+
+ else {
+   if (operator == "Scottish Premiership") (
+      op5 = "You have selected Scottish Premiership, "
+      document.getElementById(football).innerHTML = op5 + Scottish Premiership
+}
+
+ else {
+   if (operator == "La Liga") (
+       op6 = "You have selected La Liga, "
+       document.getElementById(football).innerHTML = op6 + La Liga
+
+   else {
+   if (operator == "Serie A") (
+      op7 = "You have selected Serie A, "
+      document.getElementById(football).innerHTML = op7 + Serie A
+
+  else {
+    if (operator == "Bundesliga") (
+       op8 = "You have selected Bundesliga, "
+        document.getElementById(football).innerHTML = op8 + Bundesliga
